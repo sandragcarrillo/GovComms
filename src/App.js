73e786139eb814e089f56bf221c2cf3fcec577fe
Sandra Carrillo <sancarrillo411@gmail.com>
2023-07-22 01:25:02 +0200

    setting up: react, push api, tailwind

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-4xl">GovComms coming soon!</h1>
+        <button className="bg-green-500 text-white px-4 py-2 mt-4 rounded">
+          Connect wallet
+        </button>
+      </div>
+    );
+  };
+
+export default App;
